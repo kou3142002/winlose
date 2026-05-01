c55e2ae75f2a0db48ef34977a067727c60791887
kou3142002 <kou3142002@yahoo.co.jp>
2026-05-01 22:11:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'winlose-v4';
+const CACHE_NAME = 'winlose-v5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (e) => {
